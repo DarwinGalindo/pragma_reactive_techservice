339a91059d27f1baf01eef0199f0f1859f9340ca
Darwin Galindo <dargalindosilva@gmail.com>
2025-04-02 09:40:11 -0500

    Initial commit

---
+++ src/main/java/com/darwin/techservice/TechserviceApplication.java
@@ -0,0 +1,13 @@
+package com.darwin.techservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechserviceApplication.class, args);
+	}
+
+}
